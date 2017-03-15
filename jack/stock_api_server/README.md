2d7bae25aae9d78481d778e09e8a95991dbf72af
kaihao.chan <kaihao.chan@rakuten.com>
2017-03-15 14:42:48 +0800

    Init project by docker-compose

---
+++ jack/stock_api_server/README.md
@@ -0,0 +1,9 @@
+# Auto stock server
+
+## Environment
+- Ruby 2.1.5
+- Rails@3.2.22.1
+- Angular 1.2
+
+## Getting started - development
+1. Clone this repository and checkout to dev/jack
---
+++ jack/stock_api_server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockApiServer::Application.config.secret_token = '2dee748aea7351dffec82086e12deb88a984543403e1b9e1a99e358eab8cb0ff82ddbf9e9f2ede656cf8f38766c240cc3e3e66f01a5747c2871869447630613b'
